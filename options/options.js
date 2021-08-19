9df4091356216c156070eb013197fc743af56849
Crowdbotics <team@crowdbotics.com>
2021-08-19 10:52:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "twilight_cherry_29942Identifier",
+  url: "https://twilight_cherry_29942Identifier.botics.co",
+  api: "https://twilight_cherry_29942Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
